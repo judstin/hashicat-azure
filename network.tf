241b5c515b1407166226a7882263d501580bfb63
Justin Browning <justin.browning@arq.group>
2023-08-29 05:55:19 +0000

    added network module - fixed rg typo

--- network.tf
+++ network.tf
@@ -2,6 +2,6 @@ module "network" {
   source  = "app.terraform.io/terraform-instruqt-labs/network/azurerm"
   version = "3.5.0"
   # insert required variables here
-  resource_group_name = azurerm_resource_group.myresourcegroup.resource_group_name
+  resource_group_name = azurerm_resource_group.myresourcegroup.name
   vnet_name = "gauravs-vnet"
 }
